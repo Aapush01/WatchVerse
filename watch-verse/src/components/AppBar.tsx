9c8b5496ece8d8f0259036080e70157d24b9f509
Aapush01 <mdshahidafridia31@gmail.com>
2024-08-10 21:26:05 +0530

    add SearchBar

--- watch-verse/src/components/AppBar.tsx
+++ watch-verse/src/components/AppBar.tsx
@@ -1,8 +1,8 @@
-
+import { SearchBar } from "./SearchBar"
 export const  AppBar = () => {
     return <div className="flex justify-between">
        <div>WatchVerse</div>
-       <div>Serchbar</div>
+       <div><SearchBar/></div>
        <div>Sign in</div>
     </div>
 }
