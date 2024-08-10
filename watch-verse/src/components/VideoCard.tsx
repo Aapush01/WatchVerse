c8c5bfed3f181e0c141e01018a2d172e780d0ca2
Aapush01 <mdshahidafridia31@gmail.com>
2024-08-10 17:16:07 +0530

    making videoCard dynamic

--- watch-verse/src/components/VideoCard.tsx
+++ watch-verse/src/components/VideoCard.tsx
@@ -1,21 +1,20 @@
 
-export function VideoCrad() {
+export function VideoCrad(props: any) {
     return <div>
-        <img className="rounded-xl" src="img.jpg" alt="" />
+        <img className="rounded-xl" src={props.thumbnailImg} alt="thumbnail img" />
         <div className="grid grid-cols-12 pt-2 ">
             <div className="col-span-1">
-                <img className="rounded-full w-15 h-15 pl-1" src="img1.jpg" alt="" />
+                <img className="rounded-full w-15 h-15 pl-1" src={props.AuthorImg} alt="author img" />
             </div>
             <div className="col-span-11 pl-2">
                 <div>
-
-                Time to Level Up, WATCH THIS! Befor you are too late.
+                    {props.title}
                 </div>
             <div className="col-span-11 text-gray-400 ">
-                Shahid Afridi
+                 {props.author}
             </div>
             <div className="col-span-11 text-gray-400 ">
-                 721K views | 9 months ago
+                  {props.views} views |  {props.timestamp}
             </div>
             </div>
         </div>
