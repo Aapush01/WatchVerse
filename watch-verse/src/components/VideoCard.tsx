5fdb3a6525a834c9a1459ef8b0941c4f4a3772a7
Aapush01 <mdshahidafridia31@gmail.com>
2024-08-10 16:56:42 +0530

    video card

---
+++ watch-verse/src/components/VideoCard.tsx
@@ -0,0 +1,23 @@
+
+export function VideoCrad() {
+    return <div>
+        <img className="rounded-xl" src="img.jpg" alt="" />
+        <div className="grid grid-cols-12 pt-2 ">
+            <div className="col-span-1">
+                <img className="rounded-full w-15 h-15 pl-1" src="img1.jpg" alt="" />
+            </div>
+            <div className="col-span-11 pl-2">
+                <div>
+
+                Time to Level Up, WATCH THIS! Befor you are too late.
+                </div>
+            <div className="col-span-11 text-gray-400 ">
+                Shahid Afridi
+            </div>
+            <div className="col-span-11 text-gray-400 ">
+                 721K views | 9 months ago
+            </div>
+            </div>
+        </div>
+    </div>
+}
