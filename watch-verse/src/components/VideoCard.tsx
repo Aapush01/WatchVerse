a8118dd2f77d0f94fb9353bb04907eaef82e1d4d
Aapush01 <mdshahidafridia31@gmail.com>
2024-08-11 17:41:47 +0530

    Line code for video cards

--- watch-verse/src/components/VideoCard.tsx
+++ watch-verse/src/components/VideoCard.tsx
@@ -1,7 +1,9 @@
+import { Line } from "./Line"
 
 export function VideoCrad(props: any) {
     return <div className="p-3 cursor-pointer">
         <img className="rounded-xl" src={props.thumbnailImg} alt="thumbnail img" />
+        <Line progress={10} />
         <div className="grid grid-cols-12 pt-2 ">
             <div className="col-span-1">
                 <img className="rounded-full w-15 h-15 pl-1" src={props.AuthorImg} alt="author img" />
